da76242765714dfa41f9d5d035923c1e39d120d6
clemvst <clemence@elevatesecurity.com>
2020-10-14 11:37:44 +0200

    not working but rx and tx defined

--- SendSerial_ESP32/SendSerial_ESP32.ino
+++ SendSerial_ESP32/SendSerial_ESP32.ino
@@ -1,9 +1,13 @@
 #include "M5Atom.h"
 
+#define RXD2 22
+#define TXD2 19
+
 void setup()
 {
     M5.begin();
-    Serial1.begin(9600);
+    Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+    Serial.println("Serial Txd is on pin: "+String(TX));
 }
 
 void loop()
@@ -11,6 +15,6 @@ void loop()
     M5.update();
     if (M5.Btn.isPressed())
     {
-      Serial1.write("Hello Nono");
+      Serial2.write("Hello Nono");
     }
 }
